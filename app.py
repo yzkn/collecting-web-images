97a634ab9c41861b7d6ca19a26b76f902e3cfbd1
YA <YA-androidapp@users.noreply.github.com>
2019-07-06 19:53:45 +0900

    release
    https://collecting-web-images.herokuapp.com/

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, Markup
+from datetime import datetime
+from flask import Flask, make_response, Markup, render_template, request
 from icrawler.builtin import GoogleImageCrawler
 import hashlib
-import zipfile
+import os
+import shutil
+
+
+datadir = 'data'
+maximg = 200
+
 
 app = Flask(__name__)
 
@@ -22,35 +29,64 @@ def test():
         if term == '':
             return render_template('index.html')
         else:
-            return render_template('index.html', term=term)
+            return collect(term)
     except Exception as e:
-        return str(e)
+        # return str(e)
+        pass
 
 
 @app.route('/search/<term>')
 def hello(term=''):
-    if term == '':
-        return render_template('index.html')
-    else:
-        return render_template('index.html', term=term)
+    try:
+        if term == '':
+            return render_template('index.html')
+        else:
+            return collect(term)
+    except Exception as e:
+        # return str(e)
+        pass
 
 def collect(term=''):
+    if os.path.exists(datadir):
+        shutil.rmtree(datadir)
+    os.makedirs(datadir)
+
     if term != '':
+        print('term: '+ term)
         hs = hashlib.sha256(term.encode()).hexdigest()
-        crawler = GoogleImageCrawler(storage={'root_dir': hs})
-        crawler.crawl(keyword=term, max_num=100)
+        dtstr = datetime.now().strftime('%Y%m%d%H%M%S')
+
+        imgdir  = datadir + os.path.sep + hs # dir
+        print('imgdir: '+ imgdir)
+        basename = datadir + os.path.sep + ( hs + '_' + dtstr ) # filename - ext
+        print('basename: '+ basename)
+        filename = basename + '.zip' # filename
+        print('filename: '+ filename)
+        downloadfilename = dtstr + '.zip' # header
+        print('downloadfilename: '+ downloadfilename)
+
+        # Collect
+        crawler = GoogleImageCrawler(storage={'root_dir': imgdir})
+        print('GoogleImageCrawler')
+        crawler.crawl(keyword=term, max_num=maximg)
+        print('crawler: ' + term + ' : ' + str(maximg))
 
         # zip
+        shutil.make_archive(basename, 'zip', root_dir=imgdir)
+        print('make_archive')
+
+        shutil.rmtree(imgdir)
+        print('rmtree')
 
         # Download
         response = make_response()
-        filename = hs + '.zip'
+        print('make_response')
         response.data = open(filename, 'rb').read()
-        downloadFileName = 'download.zip'
-        response.headers['Content-Disposition'] = 'attachment; filename=' + downloadFileName
-
-        # ★ポイント4
-        response.mimetype = XLSX_MIMETYPE
+        print('open')
+        response.headers['Content-Disposition'] = 'attachment; filename=' + downloadfilename
+        print('Content-Disposition')
+        response.mimetype = 'application/zip'
+        print('mimetype')
         return response
 
 if __name__ == '__main__':
